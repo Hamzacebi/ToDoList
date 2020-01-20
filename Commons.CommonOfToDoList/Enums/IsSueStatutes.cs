11d1795273d005ff504dcb60c59868ee490538a6
Sinan Hamzaçebi <sinan.hamzacebi@gmail.com>
2020-01-20 23:09:02 +0300

    IsSueStatuses and PriorityTypes(oncelik) enumlari eklendi

---
+++ Commons.CommonOfToDoList/Enums/IsSueStatutes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commons.CommonOfToDoList.Enums
+{
+    /// <summary>
+    /// Herhangi bir To Do icin islem turleri
+    /// </summary>
+    public enum IsSueStatutes
+    {
+        //ToDo :  beklemede islem tipini buraya koy
+        //https://confluence.atlassian.com/adminjiracloud/issue-statuses-priorities-and-resolutions-973500867.html
+
+        /// <summary>
+        /// Acildi ve beklemede
+        /// </summary>
+        OpenAndPending = 0,
+
+        /// <summary>
+        /// Kabul edildi
+        /// </summary>
+        Accepted = 1,
+
+        /// <summary>
+        /// Islemde - Yapiliyor - Devam etmekte
+        /// </summary>
+        InProgress = 2,
+
+        /// <summary>
+        /// Tamamlandi
+        /// </summary>
+        Done = 3,
+
+        /// <summary>
+        /// Kapatildi
+        /// </summary>
+        Closed = 4,
+
+        /// <summary>
+        /// Iptal edildi
+        /// </summary>
+        Canceled = 5,
+
+        /// <summary>
+        /// Red edildi
+        /// </summary>
+        Rejected = 6
+    }
+}
---
+++ Commons.CommonOfToDoList/Enums/PriorityType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commons.CommonOfToDoList.Enums
+{
+    /// <summary>
+    /// Acilan To Do icin oncelik turleri
+    /// </summary>
+    public enum PriorityType
+    {
+        /// <summary>
+        /// Dusuk oncelik
+        /// </summary>
+        Low = 0,
+
+        /// <summary>
+        /// Orta oncelik
+        /// </summary>
+        Medium = 1,
+
+        /// <summary>
+        /// Yuksek oncelik
+        /// </summary>
+        High = 2,
+
+        /// <summary>
+        /// Acil oncelik
+        /// </summary>
+        Urgent = 3,
+    }
+}
