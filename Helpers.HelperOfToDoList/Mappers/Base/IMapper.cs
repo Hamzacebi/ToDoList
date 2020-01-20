1ce6eb05d9b6611c5517836926a0db2498f2a3ab
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-01-20 16:05:24 +0300

    IMapper interface added

---
+++ Helpers.HelperOfToDoList/Mappers/Base/IMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers.HelperOfToDoList.Mappers.Base
+{
+    public interface IMapper<TEntity, TDTO> where TEntity : class where TDTO : class
+    {
+        /// <summary>
+        /// Verilen DTO nesnesini Entity nesnesi olarak veren fonksiyon
+        /// </summary>
+        /// <param name="dtoObject">Entity nesnesine donusturulecek olan DTO nesnesi</param>
+        /// <returns></returns>
+        TEntity MapToEntity(TDTO dtoObject);
+
+        /// <summary>
+        /// Verilen Entity nesnesini DTO nesnesi olarak veren fonksiyon
+        /// </summary>
+        /// <param name="entityObject">DTO nesnesine donusturulecek olan Entity nesnesi</param>
+        /// <returns></returns>
+        TDTO MapToDTO(TEntity entityObject);
+
+        /// <summary>
+        /// Verilen Entity nesnesine ait listeyi DTO nesnesine ait liste olarak veren fonksiyon
+        /// </summary>
+        /// <param name="entityList">DTO nesnesine ait liste olarak elde edilmek istenilen Entity nesnesine ait liste</param>
+        /// <returns></returns>
+        List<TDTO> MapToDTOList(IEnumerable<TEntity> entityList);
+    }
+}
