6c590cd050a98b44460134880de7b1dd1fc4cb28
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-01-20 10:51:01 +0300

    Geriye deger donduren ve geriye deger dondurmeyen fonksiyonlar icin Action ve Func classlarina extensionlar yazildi

---
+++ Helpers.HelperOfToDoList/Extensions/ExtensionsOfAction.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers.HelperOfToDoList.Extensions
+{
+    /// <summary>
+    /// Geriye herhangi bir deger dondurmeyen islemler icin kullanilacak olan Try - Catch - Finally fonksiyonlarini iceren Extension
+    /// </summary>
+    public static class ExtensionsOfAction
+    {
+        /// <summary>
+        /// Calismasi sonucunda geriye herhangi bir deger dondurmeyecek olan fonksiyonlarda Try - Catch islemleri yapmak icin kullanilir
+        /// </summary>
+        /// <param name="action">Calistirilmasi istenilen action</param>
+        /// <param name="catchAndDo">Action calisirken olusabilecek olan Exception icin yapilmak istenilen islemler</param>
+        public static void TryCatch(this Action action, Func<Exception, bool> catchAndDo = null)
+        {
+            try
+            {
+                action?.Invoke();
+            }
+            catch (Exception exception)
+            {
+                bool reThrow = catchAndDo?.Invoke(exception) ?? true;
+                if (reThrow)
+                {
+                    throw exception;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calismasi sonucunda geriye herhangi bir deger dondurmeyecek olan fonksiyonlarda Try - Catch - Finally islemleri yapmak icin kullanilir.
+        /// </summary>
+        /// <param name="action">Calistirilmasi istenilen action</param>
+        /// <param name="catchAndDo">Function calisirken olusabilecek olan Exception icin yapilmak istenilen islemler</param>
+        /// <param name="finallyDo">Try - Catch icin Finally aninda yapilmasi istenilen islemler</param>
+        public static void TryCatch(this Action action, Func<Exception, bool> catchAndDo = null, Action finallyDo = null)
+        {
+            try
+            {
+                action?.Invoke();
+            }
+            catch (Exception exception)
+            {
+                bool reThrow = catchAndDo?.Invoke(exception) ?? true;
+                if (reThrow)
+                {
+                    throw exception;
+                }
+            }
+            finally
+            {
+                if (finallyDo != null)
+                {
+                    finallyDo.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ Helpers.HelperOfToDoList/Extensions/ExtensionsOfFunction.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers.HelperOfToDoList.Extensions
+{
+    /// <summary>
+    /// Geriye typeof(T) tipinde bir deger dondurmesi istenilen islemler icin kullanilacak olan Try - Catch - Finally fonksiyonlarini iceren Extension
+    /// </summary>
+    public static class ExtensionsOfFunction
+    {
+        /// <summary>
+        /// Calismasi sonucunda geriye T tipinde deger dondurecek olan fonksiyonlarda Try - Catch islemi yapilmak icin kullanilir
+        /// </summary>
+        /// <typeparam name="T">Geriye dondurulecek olan veri tipi</typeparam>
+        /// <param name="function">Calistirilmasi istenilen fonksiyon</param>
+        /// <param name="catchAndDo">Function calisirken olusabilecek olan Exception icin yapilmak istenilen islemler</param>
+        /// <returns></returns>
+        public static T TryCatch<T>(this Func<T> function, Func<Exception, bool> catchAndDo = null)
+        {
+            try
+            {
+                if (function != null)
+                {
+                    return function();
+                }
+            }
+            catch (Exception exception)
+            {
+                bool reThrow = catchAndDo?.Invoke(exception) ?? true;
+                if (reThrow)
+                {
+                    throw exception;
+                }
+            }
+            return default(T);
+        }
+
+        /// <summary>
+        /// Calismasi sonucunda geriye T tipinde deger dondurecek olan fonksiyonlarda Try-Catch-Finally islemi yapmak icin kullanilir
+        /// </summary>
+        /// <typeparam name="T">Geriye dondurulecek olan veri tipi</typeparam>
+        /// <param name="function">Calistirilmasi istenilen function</param>
+        /// <param name="catchAndDo">Function calisirken olusabilecek olan Exception icin yapilmak istenilen islemler</param>
+        /// <param name="finallyDo">Try - Catch icin Finally aninda yapilmasi istenilen islemler</param>
+        /// <returns></returns>
+        public static T TryCatch<T>(this Func<T> function, Func<Exception, bool> catchAndDo = null, Action finallyDo = null)
+        {
+            try
+            {
+                if (function != null)
+                {
+                    return function();
+                }
+            }
+            catch (Exception exception)
+            {
+                bool reThrow = catchAndDo?.Invoke(exception) ?? true;
+                if (reThrow)
+                {
+                    throw exception;
+                }
+            }
+            finally
+            {
+                if (finallyDo != null)
+                {
+                    finallyDo.Invoke();
+                }
+            }
+            return default(T);
+        }
+    }
+}
