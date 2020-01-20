2896cc28c2d04d94950a7b47da5e8225a45d5bb5
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-01-20 16:09:20 +0300

    UserMapper  class added. Some other project referance added

---
+++ Helpers.HelperOfToDoList/Mappers/UserMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#region Global Usings
+//using 
+using Models.OtherModels.NeccesaryModelsOfToDoList.ModelsOfDataTransferObject;
+#endregion Global Usings
+
+namespace Helpers.HelperOfToDoList.Mappers
+{
+    #region Internal Project Using
+    using Base;
+    #endregion Internal Project Using
+
+    public class UserMapper // : IMapper<>
+    {
+
+        
+
+    }
+}
