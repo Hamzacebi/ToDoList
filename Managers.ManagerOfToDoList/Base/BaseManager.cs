a1287d2776e950f12108a9dd2ee74cc944e8bd9c
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-01-20 16:00:32 +0300

    BaseManager class added. Some Other project dependencies added

---
+++ Managers.ManagerOfToDoList/Base/BaseManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#region Global Usings
+using Microsoft.EntityFrameworkCore;
+using Commons.CommonOfToDoList.Constants;
+using DataAccess.DataAccessOfToDoList.Abstracts.UnitOfWork;
+using DataAccess.DataAccessOfToDoList.Concretes.UnitOfWork;
+#endregion Global Usings
+
+namespace Managers.ManagerOfToDoList.Base
+{
+    public abstract class BaseManager
+    {
+        #region Global Properties
+
+        private IUnitOfWork unitOfWork;
+        private readonly DbContext DbContext;
+
+        private readonly object lockObjectForUnitOfWork;
+
+
+        #endregion Global Properties
+
+        #region Constructor(s)
+
+        protected BaseManager(DbContext dbContext)
+        {
+            this.DbContext = dbContext ?? throw new ArgumentNullException(message: ConstantsOfError.ArgumentNullExceptionMessageForDbContext,
+                                                                          innerException: null);
+
+            this.lockObjectForUnitOfWork = new object();
+        }
+
+        protected IUnitOfWork UnitOfWork
+        {
+            get
+            {
+                if (this.unitOfWork == null)
+                {
+                    lock (this.lockObjectForUnitOfWork)
+                    {
+                        if (this.unitOfWork == null)
+                        {
+                            this.unitOfWork = new UnitOfWork(dbContext: this.DbContext);
+                        }
+                    }
+                }
+                return this.UnitOfWork;
+            }
+        }
+
+        #endregion Constructor(s)
+
+    }
+}
