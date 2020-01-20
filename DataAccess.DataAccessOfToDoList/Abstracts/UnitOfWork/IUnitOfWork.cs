7654d4befad6342a96175acbc1a9b860a59ab365
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-01-20 09:26:02 +0300

    Transaction, Repository ve SaveChanges fonksiyonlari ve gerekli summary ler eklendi

--- DataAccess.DataAccessOfToDoList/Abstracts/UnitOfWork/IUnitOfWork.cs
+++ DataAccess.DataAccessOfToDoList/Abstracts/UnitOfWork/IUnitOfWork.cs
@@ -4,8 +4,75 @@ using System.Text;
 
 namespace DataAccess.DataAccessOfToDoList.Abstracts.UnitOfWork
 {
+    #region Internal Project Usings
+
+    using RepositoriesOfEntities;
+
+    #endregion Internal Project Usings
+
+    /// <summary>
+    /// Repository Interfacelerine, Database Transaction fonksiyonlarina ve SaveChanges fonksiyon / fonksiyonlarina ulasmayi saglayan Interface
+    /// </summary>
     public interface IUnitOfWork
     {
-       
+        #region SaveChanges Function
+
+        /// <summary>
+        /// Database icin yapilan C (Create) - U (Update) - D (Delete) islemlerinin Database'e gonderilmesini saglayan fonksiyon
+        /// </summary>
+        /// <returns></returns>
+        int SaveChanges();
+
+        #endregion SaveChanges Function
+
+
+        #region Transaction Functions
+
+        /// <summary>
+        /// Database uzerinde Insert, Update ve Delete islemleri yapilirken olusabilecek herhangi bir hata - basari durumunda 
+        /// RollBack - Commit yonetimini yapabilmek icin Database'e ulasilma aninda direkt olarak Begin Transaction baslatan fonksiyon
+        /// </summary>
+        /// <returns></returns>
+        void BeginTransaction();
+
+        /// <summary>
+        /// Database uzerinde yapilan Insert - Update ve Delete islemleri icin herhangi bir hata durumu yoksa yapilan degisiklikleri veritabanina 
+        /// kayit edilmesini saglayan fonksiyon
+        /// </summary>
+        void CommitTransaction();
+
+        /// <summary>
+        /// Database uzerinde yapilan Insert - Update ve Delete islemleri icin yapilan islemlerde herhangi bir hata oldugunda tum degisiklikleri geri
+        /// almaya yarayan fonksiyon
+        /// </summary>
+        void RollbackTransaction();
+
+        #endregion Transaction Functions
+
+
+        #region Repositories Properties
+
+        /// <summary>
+        /// typeof(Users) Entity - Tablosu'na ait Repository tablosuna ulasmayi saglayan property
+        /// </summary>
+        IRepositoryOfUser RepositoryOfUser { get; }
+
+        /// <summary>
+        /// typeof(Categories) Entity - Tablosu'na ait Repository tablosuna ulasmayi saglayan property
+        /// </summary>
+        IRepositoryOfCategory RepositoryOfCategory { get; }
+
+        /// <summary>
+        /// typeof(ThingsToDo) Entity - Tablosu'na ait Repository tablosuna ulasmayi saglayan property
+        /// </summary>
+        IRepositoryOfThingToDo RepositoryOfThingToDo { get; }
+
+        /// <summary>
+        /// typeof(AssignmentHistoryOfTasks) Entity - Tablosu'na ait Repository tablosuna ulasmayi saglayan property
+        /// </summary>
+        IRepositoryAssignmentHistoryOfTask RepositoryAssignmentHistoryOfTask { get; }
+
+        #endregion Repositories Properties
+
     }
 }
