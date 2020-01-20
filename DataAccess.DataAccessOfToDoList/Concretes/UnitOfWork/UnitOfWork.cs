7b349b700cc438cbf475c1dc7653434bd6b03780
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-01-20 09:02:19 +0300

    IUnitOfWork ve UnitOfWork eklendi

---
+++ DataAccess.DataAccessOfToDoList/Concretes/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.DataAccessOfToDoList.Concretes.UnitOfWork
+{
+    #region Internal Project Usings
+    using Abstracts.UnitOfWork;
+    #endregion Internal Project Usings
+
+    public class UnitOfWork : IUnitOfWork
+    {
+
+
+        
+    }
+}
