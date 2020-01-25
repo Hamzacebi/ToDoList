d2f15425421b49ed72a7afc6dcde1d4671ac937e
Sinan Hamzaçebi <sinan.hamzacebi@gmail.com>
2020-01-25 22:12:32 +0300

    WebAPI modelleri icin gerekli Validasyon Mesajlari eklendi

---
+++ Commons.CommonOfToDoList/Constants/ConstantsOfValidation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commons.CommonOfToDoList.Constants
+{
+    /// <summary>
+    /// typeof(Users), typeof(Categories), typeof(ThingsToDo) ve typeof(AssignmentHistoryOfTasks) Entity'lerine ait alanlara ait Validation Error mesajlarini iceren class
+    /// </summary>
+    public class ConstantsOfValidation
+    {
+        #region Users
+
+        public const string UserNameCannotBeEmpty = "Kullanici Adi alani bos gecilemez!";
+        public const string UserEmailCannotBeEmpty = "Kullanici Email alani bos gecilemez!";
+        public const string UserSurnameCannotBeEmpty = "Kullanici Soyadi alani bos gecilemez!";
+        public const string UserPasswordCannotBeEmpty = "Kullanici Sifre alani bos gecilemez!";
+
+        #endregion Users
+
+        #region Categories
+
+        #endregion Categories
+
+        #region ThingsToDo
+
+        #endregion ThingsToDo
+
+        #region AssignmentHistoryOfTasks
+
+        #endregion AssignmentHistoryOfTasks
+    }
+}
