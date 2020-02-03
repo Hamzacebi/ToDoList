b089aca1442d305ab5578bd0808f5b9acba4ab89
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-02-03 11:11:06 +0300

    UserId validation message added

--- Models.OtherModels.NeccesaryModelsOfToDoList/ModelsOfWebAPI/WebAPIModelsOfUser/WebAPIModelOfUpdateUser.cs
+++ Models.OtherModels.NeccesaryModelsOfToDoList/ModelsOfWebAPI/WebAPIModelsOfUser/WebAPIModelOfUpdateUser.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+
+#region Global Usings
+using Commons.CommonOfToDoList.Constants;
+#endregion Global Usings
 
 namespace Models.OtherModels.NeccesaryModelsOfToDoList.ModelsOfWebAPI.WebAPIModelsOfUser
 {
     public class WebAPIModelOfUpdateUser : BaseUserWebAPIModel
     {
+        [Required(ErrorMessage = ConstantsOfValidation.UserIdCannotBeEmpty)]
         public Guid UserId { get; set; }
         public bool UserStatus { get; set; }
     }
