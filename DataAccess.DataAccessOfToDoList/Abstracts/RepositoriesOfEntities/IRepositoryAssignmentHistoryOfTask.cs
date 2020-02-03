43a7dd726cd3cbee54c38835dd1b0000a716ae1f
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-02-03 11:47:04 +0300

    Summary ve implementation lar yapildi

--- DataAccess.DataAccessOfToDoList/Abstracts/RepositoriesOfEntities/IRepositoryAssignmentHistoryOfTask.cs
+++ DataAccess.DataAccessOfToDoList/Abstracts/RepositoriesOfEntities/IRepositoryAssignmentHistoryOfTask.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
+#region Global Usings
+using Models.EntitiesOfProjects.EntitiesOfToDoList.DatabaseEntities;
+using SinanHamzaceBi.GenericRepository.Abstracts.RepositoryOfBasics;
+#endregion Global Usings
+
 namespace DataAccess.DataAccessOfToDoList.Abstracts.RepositoriesOfEntities
 {
     /// <summary>
     /// typeof(AssignmentHistoryOfTasks) Entity - Tablosu'na ait Base ve Custom fonksiyonlarin oldugu Interface
     /// </summary>
-    public interface IRepositoryAssignmentHistoryOfTask : IDisposable
+    public interface IRepositoryAssignmentHistoryOfTask : IRepositoryOfSelectable<AssignmentHistoryOfTasks>, IRepositoryOfInsertable<AssignmentHistoryOfTasks>,
+                                                          IRepositoryOfUpdatable<AssignmentHistoryOfTasks>, IDisposable
     {
     }
 }
