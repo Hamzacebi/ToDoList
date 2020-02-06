1ff749c0255c1c81668c65a406a8eecb7535aaff
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-02-06 16:20:21 +0300

    typeof(Categories) icin ResultModel classlari eklendi

---
+++ Models.OtherModels.NeccesaryModelsOfToDoList/ModelsOfWebAPI/WebAPIModelsOfCategory/ResultModelOfInsertCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.OtherModels.NeccesaryModelsOfToDoList.ModelsOfWebAPI.WebAPIModelsOfCategory
+{
+    public sealed class ResultModelOfInsertCategory
+    {
+        public ResultModel SuccessInformation { get; set; }
+        public WebAPIModelOfInsertCategory CategoryInformation { get; set; }
+    }
+}
