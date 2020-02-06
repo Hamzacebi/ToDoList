2e9fac08f328ff68e594a2c8c4fc7d01835e06f0
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-02-06 16:19:18 +0300

    typeof(Categories) icin foksiyonlar yazilmaya baslandi

--- Managers.ManagerOfToDoList/Abstracts/IManagerOfCategory.cs
+++ Managers.ManagerOfToDoList/Abstracts/IManagerOfCategory.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
+#region Global Usings
+using Models.OtherModels.NeccesaryModelsOfToDoList;
+using Models.OtherModels.NeccesaryModelsOfToDoList.ModelsOfWebAPI.WebAPIModelsOfCategory;
+#endregion Global Usings
+
 namespace Managers.ManagerOfToDoList.Abstracts
 {
     public interface IManagerOfCategory
     {
+        ResultModelOfInsertCategory CreateNewCategory(WebAPIModelOfInsertCategory categoryToInsert);
     }
 }
--- Managers.ManagerOfToDoList/Concretes/ManagerOfCategory.cs
+++ Managers.ManagerOfToDoList/Concretes/ManagerOfCategory.cs
@@ -1,12 +1,63 @@
-﻿namespace Managers.ManagerOfToDoList.Concretes
+﻿using System;
+
+#region Global Usings
+using Commons.CommonOfToDoList.Constants;
+using Helpers.HelperOfToDoList.Extensions;
+using Models.OtherModels.NeccesaryModelsOfToDoList;
+using Models.OtherModels.NeccesaryModelsOfToDoList.ModelsOfWebAPI.WebAPIModelsOfCategory;
+#endregion Global Usings
+
+namespace Managers.ManagerOfToDoList.Concretes
 {
     #region Internal Project Usings
     using Base;
     using Abstracts;
     #endregion Internal Project Usings
 
+    /// <summary>
+    /// typeof(Categories) tablosuna ait BusinessManager classi 
+    /// </summary>
     public class ManagerOfCategory : BaseManager, IManagerOfCategory
     {
 
+        private readonly IManagerOfUser userManager;
+
+        public ManagerOfCategory()
+        {
+            this.userManager = new ManagerOfUser();
+        }
+
+        ResultModelOfInsertCategory IManagerOfCategory.CreateNewCategory(WebAPIModelOfInsertCategory categoryToInsert)
+        {
+            ResultModel resultToReturn = default(ResultModel);
+            int numberOfRowsAffected = default(int);
+            try
+            {
+                var isThereAnyUser = this.userManager
+                                          .FecthUserById(userId: categoryToInsert.CategoryOwnerUserId);
+                if (isThereAnyUser.SuccessInformation.IsSuccess)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception exception)
+            {
+                this.UnitOfWork.RollbackTransaction();
+                resultToReturn = ResultModel.UnsuccessfulResult(unsuccessfulResultMessage: $"{ConstantsOfErrors.CreateNewCategoryTransactionErrorMessage} HATA: ${exception.Message}");
+            }
+            finally
+            {
+                this.UnitOfWork.Dispose();
+            }
+            return new ResultModelOfInsertCategory()
+            {
+                SuccessInformation = resultToReturn,
+                CategoryInformation = categoryToInsert
+            };
+        }
     }
 }
