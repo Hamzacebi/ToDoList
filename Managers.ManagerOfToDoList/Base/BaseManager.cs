58109719724ac7445d1eec541c7494f9337bdc66
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-02-27 17:31:36 +0300

    Mapperler icin tek tek yazilmis olan singleton classlar generic hale getirildi

--- Managers.ManagerOfToDoList/Base/BaseManager.cs
+++ Managers.ManagerOfToDoList/Base/BaseManager.cs
@@ -15,25 +15,10 @@ namespace Managers.ManagerOfToDoList.Base
 {
     public abstract class BaseManager : IDisposable
     {
-        #region Global Properties
-        private readonly object userMapperOfLocObject, categoryMapperOfLockObject,
-                                thingToDoOfLockObject, lockObjectForAssignmentHistoryOfTask;
-
-        private IMapper<Users, DTOOfUser> userMapper;
-        private IMapper<Categories, DTOOfCategory> categoryMapper;
-        private IMapper<ThingsToDo, DTOOfThingToDo> thingToDoMapper;
-        private IMapper<AssignmentHistoryOfTasks, DTOOfAssignmentHistoryOfTask> assignmentHistoryOfTaskMapper;
-        #endregion Global Properties
 
         #region Constructor(s)
 
-        protected BaseManager()
-        {
-            this.userMapperOfLocObject =
-            this.thingToDoOfLockObject =
-            this.categoryMapperOfLockObject =
-            this.lockObjectForAssignmentHistoryOfTask = new object();
-        }
+        protected BaseManager() { }
 
         #endregion Constructor(s)
 
@@ -56,19 +41,7 @@ namespace Managers.ManagerOfToDoList.Base
         {
             get
             {
-                // ToDo :IMapper<TEntity,TDTO> yani generic oldugu icin cast aninda hata veriyor. cozum uretilmesi gerekiyor
-                //return UtilityTools.CreateGenericSingletonInstance<IMapper<Users, DTOOfUser>>(resultToReturnClass: typeof(UserMapper));
-                if (this.userMapper == null)
-                {
-                    lock (this.userMapperOfLocObject)
-                    {
-                        if (this.userMapper == null)
-                        {
-                            this.userMapper = new UserMapper();
-                        }
-                    }
-                }
-                return this.userMapper;
+                return UtilityTools.CreateGenericSingletonInstance<IMapper<Users, DTOOfUser>>(resultToReturnClass: typeof(UserMapper));
             }
         }
 
@@ -76,17 +49,7 @@ namespace Managers.ManagerOfToDoList.Base
         {
             get
             {
-                if (this.categoryMapper == null)
-                {
-                    lock (this.categoryMapperOfLockObject)
-                    {
-                        if (this.categoryMapper == null)
-                        {
-                            this.categoryMapper = new CategoryMapper();
-                        }
-                    }
-                }
-                return this.categoryMapper;
+                return UtilityTools.CreateGenericSingletonInstance<IMapper<Categories, DTOOfCategory>>(resultToReturnClass: typeof(CategoryMapper));
             }
         }
 
@@ -94,17 +57,7 @@ namespace Managers.ManagerOfToDoList.Base
         {
             get
             {
-                if (this.thingToDoMapper == null)
-                {
-                    lock (this.thingToDoOfLockObject)
-                    {
-                        if (this.thingToDoMapper == null)
-                        {
-                            this.thingToDoMapper = new ThingToDoMapper();
-                        }
-                    }
-                }
-                return this.thingToDoMapper;
+                return UtilityTools.CreateGenericSingletonInstance<IMapper<ThingsToDo, DTOOfThingToDo>>(resultToReturnClass: typeof(ThingToDoMapper));
             }
         }
 
@@ -112,17 +65,8 @@ namespace Managers.ManagerOfToDoList.Base
         {
             get
             {
-                if (this.assignmentHistoryOfTaskMapper == null)
-                {
-                    lock (this.lockObjectForAssignmentHistoryOfTask)
-                    {
-                        if (this.assignmentHistoryOfTaskMapper == null)
-                        {
-                            this.assignmentHistoryOfTaskMapper = new AssignmentHistoryOfTaskMapper();
-                        }
-                    }
-                }
-                return this.assignmentHistoryOfTaskMapper;
+                return UtilityTools.CreateGenericSingletonInstance<IMapper<AssignmentHistoryOfTasks,
+                                                                           DTOOfAssignmentHistoryOfTask>>(resultToReturnClass: typeof(AssignmentHistoryOfTaskMapper));
             }
         }
         #endregion Mapper Properties
